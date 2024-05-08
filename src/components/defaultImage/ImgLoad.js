dc997373d7c55846723b028f204d28e5dafc87fc
seongyunlee <private.seongyun@gmail.com>
2024-05-08 16:39:51 +0000

    Make some css for SearchResults.js

---
+++ src/components/defaultImage/ImgLoad.js
@@ -0,0 +1,37 @@
+import {useEffect, useRef, useState} from "react";
+import styles from "./ImgLoad.module.css";
+
+const ImgLoad = (props) => {
+
+    const {src, ..._props} = props;
+
+
+    const placeholder = "/public/person.svg";
+
+    const divRef = useRef();
+    const imgRef = useRef();
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const image = new Image();
+        image.src = "https://source.unsplash.com/random";
+        image.onload = () => {
+            setIsLoading(false);
+        }
+    }, []);
+    return (
+        <div>
+            {
+                isLoading ? (
+                    <div className={styles.imgLoad} ref={divRef}>
+                    </div>
+                ) : (
+                    <img src={src} {..._props} ref={imgRef}
+                    ></img>
+                )}
+        </div>
+    );
+}
+
+export default ImgLoad;
--- src/pages/search/SearchResults.js
+++ src/pages/search/SearchResults.js
@@ -1,17 +1,35 @@
-import styles from "./Search.module.css";
 import axios from "axios";
-import {useState, useContext, useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import styles from "./Search.module.css";
 
 const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOjE3MTUxNTgwNzksImV4cCI6MTc0NjY5NDA3OSwiYXVkIjoiIiwic3ViIjoidGVzdHVzZXIifQ._zVQhiluqkNvElvU45WPH2gaoPB7J_c_ZvTOU3zqvD0"
 const SearchBox = ({value, onChange}) => {
     return (
-        <input
-        type="search"
-        placeholder="Search..."
-        value={value}
-        onChange={onChange}
-      />
+        <div className={styles.searchBox}>
+            <img src="/search.svg" alt="search"/>
+            <input
+                className={styles.searchBoxInput}
+                type="search"
+                placeholder="Search..."
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    );
+}
+const ResultItem = (props) => {
+
+    const {onClick, result} = props;
+
+    return (
+        <div className={styles.resultItem} onClick={onClick}>
+            <img src={result.coverImageUrl} className={styles.resultItemImg}/>
+            <div>
+                <div className={styles.resultItemName}>{result.name}</div>
+                <div className={styles.resultItemArtist}>{result.artistName}</div>
+            </div>
+        </div>
     );
 }
 
@@ -24,39 +42,27 @@ const SearchResults = ({results, searching}) => {
     };
     // 우선 앨범 결과만 온다고 가정
     return (
-        <article aria-busy={searching}>
-            {searching ? (
-                "Loading..."
-            ) : (
-                <>
-                <ul>
-                    {results.map((result) => (
-                        <li key={result.id} onClick={() => handleItemClick(result.id)}>
-                            <h3>{result.name}</h3>
-                            <p>{result.artistName}</p>
-                        </li>
-                    ))}                    
-                 
-                </ul>
-                </>
-            )}
-        </article>
+        <div className={styles.searchResults}>
+            {searching ? "Loading..." : ""}
+            {results.map((result) => (
+                <ResultItem key={result.id} result={result} onClick={() => handleItemClick(result.id)}/>
+            ))}
+        </div>
     );
 }
 
 const fetchResults = async ({query}) => {
     if (!query) return [];
     try {
-        
-        const response = await axios.get(`${process.env.REACT_APP_API_HOST}/album/search?keyword=${query}`, {
-        });
-      // console.log(response.data);
-      return response.data;
+
+        const response = await axios.get(`${process.env.REACT_APP_API_HOST}/album/search?keyword=${query}`, {});
+        // console.log(response.data);
+        return response.data;
     } catch (error) {
-      console.error(error);
-      return [];
+        console.error(error);
+        return [];
     }
-  };
+};
 
 const useDebouncedState = (value, delay = 500) => {
     const [debouncedValue, setDebouncedValue] = useState(value);
@@ -68,34 +74,34 @@ const useDebouncedState = (value, delay = 500) => {
 
     return debouncedValue;
 }
-  
+
 const Search = () => {
     const [query, setQuery] = useState("");
     const debouncedQuery = useDebouncedState(query, 1_000);
     const [results, setResults] = useState([]);
     const [searching, setSearching] = useState(false);
-  
-    useEffect(() => {
-      setSearching(true);
-      // console.log(debouncedQuery);
 
-      (async () => {
-        let results = await fetchResults({query: debouncedQuery});
-        while (results.length === 0) {
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-            //console.log(results)
-            results = await fetchResults({query: debouncedQuery});
-        }
-        setResults(results);
-        setSearching(false);
-    })();
+    useEffect(() => {
+        setSearching(true);
+        // console.log(debouncedQuery);
+
+        (async () => {
+            let results = await fetchResults({query: debouncedQuery});
+            while (results.length === 0) {
+                await new Promise((resolve) => setTimeout(resolve, 1000));
+                //console.log(results)
+                results = await fetchResults({query: debouncedQuery});
+            }
+            setResults(results);
+            setSearching(false);
+        })();
     }, [debouncedQuery]);
-  
+
     return (
-      <>
-        <SearchBox value={query} onChange={(e) => setQuery(e.target.value)} />
-        <SearchResults results={results} searching={searching} />
-      </>
+        <div className={styles.container}>
+            <SearchBox value={query} onChange={(e) => setQuery(e.target.value)}/>
+            <SearchResults results={results} searching={searching}/>
+        </div>
     );
 }
 
