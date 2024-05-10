84dd1123a691787376a5bfa7f263f00f0517a98b
seongyunlee <private.seongyun@gmail.com>
2024-05-10 08:39:33 +0000

    아이콘 축소

--- src/pages/albumDetail/AlbumDetail.js
+++ src/pages/albumDetail/AlbumDetail.js
@@ -11,39 +11,35 @@ import ToggleFilter from "../../components/toggleFilter/ToggleFilter";
 import TrackReview from "../../components/trackReview/TrackReview";
 
 const MainPage = () => {
+    const onContainerClick = () => {
+    };
+    
     return (
         <div className="mainPage">
-            <h2>수록곡</h2>
-            <div className="trackList">
-                <div className="track">
-                    <span className="trackNumber">1</span>
-                    <span className="trackName">클린카드 (Queencard)</span>
-                    <span className="trackTime">03:48</span>
-                    <span className="trackRating">4.2 / 5</span>
-                </div>
-                {/* Repeat for other tracks */}
-            </div>
-
-            <h2>탑리뷰</h2>
-            <div className="userReview">
-                <img src="user-profile.jpg" alt="User profile" className="userImage"/>
-                <div className="reviewContent">
-                    <div className="userName">아침나무늘보님</div>
-                    <div className="reviewDate">작성일 2024.04.01</div>
-                    <div className="reviewText">내가 봤던 최고의!</div>
-                    <div className="reviewLikes">
-                        <span className="heartIcon">❤️</span>
-                        <span>76</span>
+            <section className={styles.homeSection}>
+                <div className={styles.sectionTitle}>
+                    <h2>수록곡</h2>
+                    <div className={styles.toggleContainer}>
+                        <img src="/arrow_down.svg" alt="arrow" className={styles.arrow} />
                     </div>
                 </div>
-            </div>
 
-            <h2>평점</h2>
-            <div className="ratingSummary">
-                <div className="ratingStars">★★★★★</div>
-                <div className="ratingCount">5</div>
-            </div>
+                <div className={styles.sectionTitle}>
+                    <h2>탑리뷰</h2>
+                    <div className={styles.toggleContainer}>
+                        <img src="/arrow_up.svg" alt="arrow" className={styles.arrow} />
+                    </div>
+                </div>
+                <div className="verticalScroll">
+                    <TrackReview/>
+                </div>
+
+                <div className={styles.sectionTitle}>
+                    <h2>별점</h2>
+                </div>
+            </section>
         </div>
+
     );
 };
 
@@ -55,7 +51,9 @@ const ReviewPage = () => {
             <section className={styles.homeSection}>
                 <div className={styles.sectionTitle}>
                     <h2>앨범리뷰</h2>
-                    <ToggleFilter menu={["최근", "인기"]}/>
+                    <div className={styles.toggleContainer}>
+                        <ToggleFilter menu={["최근", "인기"]} />
+                    </div>
                 </div>
                 <div className="verticalScroll">
                     <ReviewPreview
@@ -72,11 +70,13 @@ const ReviewPage = () => {
                         prop="아이들 리뷰 제목"
                         onContainerClick={onContainerClick}/>
                 </div>
-            </section>
 
-            <h2>수록곡 리뷰</h2>
-            <ToggleFilter menu={["최근", "인기"]}/>
-            <section className={styles.homeSection}>
+                <div className={styles.sectionTitle}>
+                    <h2>수록곡 리뷰</h2>
+                    <div className={styles.toggleContainer}>
+                        <ToggleFilter menu={["최근", "인기"]} />
+                    </div>
+                </div>
                 <div className="verticalScroll">
                     <TrackReview/>
                 </div>
@@ -91,7 +91,9 @@ const ListPage = () => {
         <section className={styles.homeSection}>
         <div className={styles.sectionTitle}>
             <h2>플레이리스트</h2>
-            <ToggleFilter menu={["최근", "인기"]}/>
+            <div className={styles.toggleContainer}>
+                <ToggleFilter menu={["최근", "인기"]} />
+            </div>
         </div>
         <div className="verticalScroll">
             <PlaylistPreview
@@ -108,6 +110,7 @@ const ListPage = () => {
     </section>
     );
 };
+
 // 앨범 상세페이지 컴포넌트
 const AlbumDetailsPage = (props) => {
     console.log(props.albumId)
@@ -137,41 +140,48 @@ const AlbumDetailsPage = (props) => {
         return <div>Loading album information...</div>; // 로딩 상태일 때 로딩 메시지 표시
     }
 
-    return (        
-      <div className={styles.albumPage}>
-        <div className={styles.albumArtContainer}>
-          <img src={albumInfo.albumImg} alt="Album Art" className={styles.albumArt} />
-        </div>
-        <div className={styles.albumInfo}>
-          <h1>{albumInfo.albumName}</h1>
-          <p>2023.05.15 | KR | 6곡 1시간 17분 소요</p>
-          <p>{albumInfo.artistName}</p>
-        </div>
-        <div className={styles.ratingInfo}>
-          <div className={styles.totalReviews}>Total Reviews 2414</div>
-          <div className={styles.averageRating}>Average Ratings 4.2 / 5</div>
-          <div className={styles.yourRating}>Your Ratings ? / 5</div>
-        </div>
-        <button className={styles.reviewButton} onClick={() => setReviewWriteModalOpen(true)}>이 앨범 리뷰하기 / 나의 리뷰 보기</button>
-        <div className={styles.socialButtons}>
-          <img src="/Vector.svg" alt="Vector" className={styles.socialIcon} />
-          <img src="/share.svg" alt="Share" className={styles.socialIcon} />
-          <img src="/bookmark_border.svg" alt="Bookmark" className={styles.socialIcon} />
-          <img src="/control_point.svg" alt="Control Point" className={styles.socialIcon} />
-          <img src="/open_in_new_off.svg" alt="Open in New" className={styles.socialIcon} />
-        </div>
-        {/* NavigationBar 컴포넌트 추가 */}
-        <NavigationBar />
-        
-        {reviewWriteModalOpen &&            
+    return (
+        <div className={styles.albumPage}>
+            <div className={styles.contentContainer}> 
+                <div className={styles.albumArtContainer}>
+                    <img src={albumInfo.albumImg} alt="Album Art" className={styles.albumArt} />
+                </div>
+                <div className={styles.albumInfo}>
+                    <h1>{albumInfo.albumName}</h1>
+                    <p>2023.05.15 | KR | 6곡 | 총 1시간 17분</p>
+                    <p>{albumInfo.artistName}</p>
+                </div>
+                <div className={styles.ratingInfo}>
+                    <div className={styles.ratingItem}>
+                        <div className={styles.value}>2414</div>
+                        <div className={styles.label}>Total Reviews</div>
+                    </div>
+                    <div className={styles.ratingItem}>
+                        <div className={styles.value}>4.2 / 5</div>
+                        <div className={styles.label}>Average Ratings</div>
+                    </div>
+                    <div className={styles.ratingItem}>
+                        <div className={styles.value}>? / 5</div>
+                        <div className={styles.label}>Your Ratings</div>
+                    </div>
+                </div>
+                
+                <button className={styles.reviewButton}>이 앨범 리뷰하기 / 나의 리뷰 보기</button>
+                <div className={styles.socialButtons}>
+                    <img src="/Vector.svg" alt="Vector" className={styles.socialIcon} />
+                    <img src="/share.svg" alt="Share" className={styles.socialIcon} />
+                    <img src="/add.svg" alt="Add" className={styles.socialIcon} />
+                </div>
+            </div>
+            <NavigationBar /> {/* This remains outside the new container */}
+            {reviewWriteModalOpen &&            
                <AlbumReviewWrite albumId={props.albumId}
                     reviewWriteModalOpen={reviewWriteModalOpen}
                     setReviewWriteModalOpen={setReviewWriteModalOpen}
                     reviewWriteModalBackground={reviewWriteModalBackground}
                />
         }
-
-      </div>
+        </div>
     );
   };
 
